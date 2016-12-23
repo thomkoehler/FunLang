301dbad0f2c047d1981290385c3efe3c28547e48
thomkoehler <thom.koehler@gmail.com>
2016-12-23 18:29:52 +0100

    FunLang

--- src/TIM/Heap.hs
+++ src/TIM/Heap.hs
@@ -1,5 +1,7 @@
 ---------------------------------------------------------------------------------------------------
 
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
 module TIM.Heap
 (
    Heap,
@@ -12,10 +14,17 @@ module TIM.Heap
 where
 
 import qualified Data.Map.Strict as Map
+import Text.PrettyPrint.GenericPretty
+import Text.PrettyPrint hiding(empty)
 
 
-data Heap a d = Heap !(Map.Map a d) !a 
+data Heap a d = Heap !(Map.Map a d) !a
    deriving(Show)
+
+instance (Out n, Out i) => Out (Map.Map n i) where
+   doc = braces . docList . Map.toList
+   docPrec _ = doc
+
 
 empty :: (Num a) => Heap a d
 empty = Heap Map.empty 1
@@ -32,7 +41,7 @@ update addr x (Heap heapMap newAddr) = Heap (Map.insert addr x heapMap) newAddr
 free :: (Ord a) => a -> Heap a d -> Heap a d
 free addr (Heap heapMap newAddr) = Heap (Map.delete addr heapMap) newAddr
 
-get :: Ord a => a -> Heap a d -> Maybe d 
+get :: Ord a => a -> Heap a d -> Maybe d
 get addr (Heap m _) = Map.lookup addr m
 
 ---------------------------------------------------------------------------------------------------
--- src/TIM/Types.hs
+++ src/TIM/Types.hs
@@ -1,9 +1,15 @@
 
+{-# LANGUAGE DeriveGeneric #-}
+
 module TIM.Types where
 
+import Text.PrettyPrint.GenericPretty
+import Text.PrettyPrint
 import qualified TIM.Heap as Heap
 import qualified Data.Text as T
 import qualified Data.Map as Map
+
+import Utils.PrettyPrint
 
 type Addr = Int
 type Name = T.Text
@@ -12,22 +18,26 @@ data Instruction
    = Take {-# UNPACK #-} !Int
    | Enter !AMode
    | Push !AMode
-   deriving(Show)
+   deriving(Show, Generic)
 
+instance Out Instruction
 
 data AMode
    = Arg {-# UNPACK #-} !Int
    | Label {-# UNPACK #-} !Name
    | Code ![Instruction]
    | IntConst {-# UNPACK #-} !Int
-   deriving(Show)
+   deriving(Show, Generic)
 
+instance Out AMode
 
 data FramePtr
    = FrameAddr {-# UNPACK #-} !Addr
    | FrameInt {-# UNPACK #-} !Int
    | FrameNull
-   deriving(Show)
+   deriving(Show, Generic)
+
+instance Out FramePtr
 
 
 type Frame = [Closure]
@@ -35,6 +45,7 @@ type Closure = ([Instruction], FramePtr)
 type TimHeap = Heap.Heap Addr Frame
 type TimStack = [Closure]
 type CodeStore = Map.Map Name [Instruction]
+
 
 data TimState = TimState
    {
@@ -44,4 +55,16 @@ data TimState = TimState
       heap :: !TimHeap,
       codeStore :: !CodeStore
    }
-   deriving(Show)
+   deriving(Show, Generic)
+
+instance Out TimState where
+   doc (TimState is fp st hp cs) = braces $ docIs <> docFp <> docSt <> docHp <> docCs
+      where
+         docIs = text "instructions: " <> doc is
+         docFp = text ", frame: " <> doc fp
+         docSt = text ", stack: " <> doc st
+         docHp = text ", heap: " <> doc hp
+         docCs = text ", code store: " <> doc cs
+
+
+   docPrec _ = doc
